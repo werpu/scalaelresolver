402073aa634d364fb46bea9c709255b78ce41e16
Werner Punz <werpu@apache.org>
2013-08-23 13:34:02 +0200

    using inject to test a mixed case

--- elresolvertest/src/main/scala/com/github/werpu/scalaelresolver/scalaelresolvertest/Bean3.scala
+++ elresolvertest/src/main/scala/com/github/werpu/scalaelresolver/scalaelresolvertest/Bean3.scala
@@ -1,15 +1,15 @@
 package com.github.werpu.scalaelresolver.scalaelresolvertest
 
-import javax.inject.Named
+import javax.inject.{Inject, Named}
 import javax.enterprise.context.RequestScoped
-import scala.reflect.BeanProperty
 
 @Named
 @RequestScoped
 class Bean3 {
-  @BeanProperty
   var test1 = "hello world from bean3"
-  @BeanProperty
   var test2 = "hello world from bean 3 test2"
+
+  @Inject
+  var bean4 : Bean4 = null
 
 }
